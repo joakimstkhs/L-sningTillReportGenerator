a48d8be0885b9e949d8a62fa27fab30b7f9a54c6
AlejandroKnightec <alejandro.amestegui@knightec.se>
2024-10-30 08:32:04 +0100

    Add project files.

---
+++ ReportGenerator/ReportGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator
+{
+    public interface IReport
+    {
+        void Generate();
+    }
+
+    public class PdfReport : IReport
+    {
+        public void Generate() => Console.WriteLine("PDF Report Generated.");
+    }
+
+    public class ExcelReport : IReport
+    {
+        public void Generate() => Console.WriteLine("Excel Report Generated.");
+    }
+
+    public class WordReport : IReport
+    {
+        public void Generate() => Console.WriteLine("Word Report Generated.");
+    }
+
+    public class ReportGenerator
+    {
+        public IReport CreateReport(string reportType)
+        {
+            if (reportType == "PDF")
+                return new PdfReport();
+            else if (reportType == "Excel")
+                return new ExcelReport();
+            else if (reportType == "Word")
+                return new WordReport();
+            else
+                throw new ArgumentException("Invalid report type");
+        }
+    }
+}
