7ac63c87c3fe238f35ffc0d0ec652dfe055c6949
AlejandroKnightec <alejandro.amestegui@knightec.se>
2024-10-30 08:32:04 +0100

    Add project files.

---
+++ ReportGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ReportGenerator
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var reportGenerator = new ReportGenerator();
+            
+            var pdfReport = reportGenerator.CreateReport("PDF");
+            pdfReport.Generate();
+
+            var excelReport = reportGenerator.CreateReport("Excel");
+            excelReport.Generate();
+
+            var wordReport = reportGenerator.CreateReport("Word");
+            wordReport.Generate();
+
+            try
+            {
+                var invalidReport = reportGenerator.CreateReport("InvalidType");
+                invalidReport.Generate();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
